d1b243b1ba5ef9f21cda86f2b0cd8e1c1624b642
lightlitha <lightlitha@gmail.com>
2025-03-30 15:42:28 +0200

    Forecast Data Table Display

--- src/shared/domain/src/hourly.ts
+++ src/shared/domain/src/hourly.ts
@@ -2,3 +2,8 @@ export interface Hourly {
   time: string[];
   temperature_2m: number[];
 }
+
+export interface HourlyForecast {
+  time: string;
+  temperature_2m: number;
+}
--- src/shared/lib/home/home.component.ts
+++ src/shared/lib/home/home.component.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { OpenMeteoForecastUriParameters, Forecast } from '@domain';
+import { Component, inject, signal } from '@angular/core';
+import { OpenMeteoForecastUriParameters, Forecast, HourlyForecast } from '@domain';
 import { CommonHelper } from '@helpers';
 import { OpenMeteoService } from '@services';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { CalendarModule } from 'primeng/calendar';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule, TableModule, CalendarModule, CardModule],
 })
 export class HomeComponent {
   openMeteoService = inject(OpenMeteoService);
 
   forecastData: Forecast | null = null;
+
+  hourlyWeather: HourlyForecast[] = [];
 
   parameters: OpenMeteoForecastUriParameters = {
     latitude: 0,
@@ -21,9 +27,17 @@ export class HomeComponent {
   };
 
   ngOnInit(): void {
+    this.WeatherReport();  
+  }
+
+  WeatherReport() {
     this.openMeteoService.weatherReport(this.parameters).subscribe({
       next: (forecast) => {
         this.forecastData = forecast;
+        this.hourlyWeather = forecast.hourly.time.map((time, index) => ({
+          time,
+          temperature_2m: forecast.hourly.temperature_2m[index]
+        }))
       },
       error: (error) => {
         CommonHelper.exceptionHandling(error);
